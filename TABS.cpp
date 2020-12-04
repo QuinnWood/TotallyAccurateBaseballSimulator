431e86ec8fde93e26f43719f2c4a5d6ec8d4631b
QuinnWood <58484953+QuinnWood@users.noreply.github.com>
2020-12-04 18:00:42 -0500

    Update Tabs.cpp
    A better a more complete version of the teambuilder menu function. Open to any suggestions on how to additionally fine tune it.

--- TABS.cpp
+++ TABS.cpp
@@ -27,9 +27,9 @@ struct Player {
     int IP = 0;    // IPOuts divided by 3
 };
 
-void openFile(string filename, vector<Player> &vect, vector<Player> &vectYears, unordered_map<string, int> &map) {
+void openFile(string filename, vector<Player>& vect, vector<Player>& vectYears, unordered_map<string, int>& map) {
     ifstream workFile;
-    workFile.open("C:\\Users\\ian\\Desktop\\TABS\\csvs\\"+filename);
+    workFile.open("C:\\Users\\ian\\Desktop\\TABS\\csvs\\" + filename);
     if (workFile.good()) {
         cout << "open" << endl;
     }
@@ -49,7 +49,7 @@ void openFile(string filename, vector<Player> &vect, vector<Player> &vectYears, 
             }
             getline(ss, first, ',');
             getline(ss, last, ',');
-            
+
             Player player;
             player.playerID = id;
             player.name = first + " " + last;
@@ -79,11 +79,11 @@ void openFile(string filename, vector<Player> &vect, vector<Player> &vectYears, 
             getline(ss, atBat, ',');
             getline(ss, garbage, ',');
             getline(ss, hits, ',');
-            
+
             vect.at(map[id]).gamesPlayed = vect.at(map[id]).gamesPlayed + stoi(games);
             vect.at(map[id]).atBats = vect.at(map[id]).atBats + stoi(atBat);
             vect.at(map[id]).hits = vect.at(map[id]).hits + stoi(hits);
-            
+
             Player playerYear;
             playerYear.playerID = id + year + team;
             playerYear.name = vect.at(map[id]).name;
@@ -129,21 +129,389 @@ void openFile(string filename, vector<Player> &vect, vector<Player> &vectYears, 
     workFile.close();
 }
 
+void playerAdder(vector<Player>& objects, vector<Player>& team) {
+    int pick; //USER PICKS THE PLAYER THEY WANT
+    bool gate = false;
+
+    for (int j = 1; j <= objects.size(); j++) {
+        cout << j << ": " << objects[--j].name << endl;
+    }
+
+    cin >> pick;
+    while (!gate) {
+        if (pick < 1 || pick > 10) {
+            cout << "Please pick a player.\n";
+            cin >> pick;
+        }
+
+        else {
+            team.push_back(objects[pick]);
+            objects.clear();
+            pick = 0;
+            gate = true;
+        }
+    }
+}
+
+//BUILDS THE VECTOR FULL OF PLAYERS FOR A TEAM
+
+void teamBuilder(vector<pair<string, vector<Player>>> pos, vector<Player> plyrYear, vector<Player> plyrs, vector<Player> &team, string teamName) {
+    int buildQuality; //DETERMINES IF USER WANTS TO SEACH FOR PLAYERS, WANTS THE BEST/WORST, OR WANTS RANDOM PLAYERS
+    int plyrStat; //DETERMINES IF THE USER WANTS THE PLAYER'S STATS TO BE OVERALL STATS OR BASED UPON A SPECIFIC YEAR
+    int plyrPos; //USER PICKS THE POSITION THAT THEY WANT THE PLAYER TO BE FROM
+    int yr; //USER ENTERS IN THE YEAR THE PLAYER THEY ARE LOOKING FOR PLAYED IN
+    int bw; //USER CHOOSES IF THEY WANT THE BEST PLAYERS OR THE WORST PLAYERS
+    int bwNum; //USER CHOOSES HOW MANY OF THE BEST/WORST PLAYERS THEY'LL HAVE TO CHOOSE FROM
+    int sorter; //USER CHOOSES THE STAT THEY WANT EVERY PLAYER TO BE SORTED BY
+    string plyrNm; //USER INPUTS THE NAME OF THE PLAYER THEY WANT
+    string plyrSortStat; //THE STRING VERSION OF THE STAT THEY WANT GENERAL PLAYERS TO BE SORTED BY
+    string pitchSortStat; //THE STRING VERSION OF THE STAT THE USER WANTS THEIR PITCHER TO BE SORTED BY
+    vector<Player> objects; //STORES ALL THE PLAYERS THAT THE USER CAN CHOOSE FROM DURING EACH SELECTION
+    bool best = false; //IF FALSE THEN THE USER WANTS THE WORST PLAYERS AND IF TRUE THEN THE USER WANTS THE BEST PLAYERS
+    bool gate = false; //ALLOWS FOR THE USE OF A WHILE LOOP WHEN USER IS CHOOSING AN OPTION IN CASE THE OPTION IS INVALID
+
+    cout << "WELCOME TO THE TEAMBUILDER\nHERE YOU WILL PICK A TEAM OF 10 PLAYERS, INCLUDING ONE PITCHER AND THE REST BEING YOUR CHOICE\n";
+
+    cout << "FIRST THING WE NEED TO \"SORT\" OUT IS WHICH STAT YOU WOULD LIKE THE PITCHERS TO BE SORTED BY?\n1: ERA\n2: ER\n3: IPOUTS\n4: IP\n";
+    cin >> sorter;
+
+    while (!gate) {
+        if (sorter == 1) {
+            pitchSortStat = "ERA";
+            gate = true;
+        }
+
+        else if (sorter == 2) {
+            pitchSortStat = "ER";
+            gate = true;
+        }
+
+        else if (sorter == 3) {
+            pitchSortStat = "IPOUTS";
+            gate = true;
+        }
+
+        else if (sorter == 4) {
+            pitchSortStat = "IP";
+            gate = true;
+        }
+
+        else {
+            cout << "Please enter a valid option.\n";
+            cin >> sorter;
+        }
+
+    }
+
+    gate = false;
+    sorter = 0;
+
+    cout << "NOW WHICH STAT WOULD YOU LIKE THE OTHER PLAYERS TO BE SORTED BY?\n1: BATTING AVERAGE\n2: HITS\n3: TIMES THEY WENT TO BAT (at bat)\n";
+    cin >> sorter;
+
+    while (!gate) {
+        if (sorter == 1) {
+            plyrSortStat = "BatAvg";
+            gate = true;
+        }
+
+        else if (sorter == 2) {
+            plyrSortStat = "HITS";
+            gate = true;
+        }
+
+        else if (sorter == 3) {
+            plyrSortStat = "AtBat";
+            gate = true;
+        }
+
+        else {
+            cout << "Please choose a valid option.";
+            cin >> sorter;
+        }
+    }
+
+
+    cout << "HOW YOU WOULD LIKE TO HAVE PLAYERS PRESENTED?\n1: GIVE ME RANDOM PLAYERS\n2: ADVANCED SEARCH\n3: BEST/WORST\n";
+    cin >> buildQuality;
+
+    switch (buildQuality) {
+
+    //RANDOMS
+    case 1:
+        cout << "RANDOM PLAYERS IT IS! WHICH WOULD YOU PREFER THE PLAYER'S STATS TO BE: OVERALL (the average for all the years they played), OR THEIR STATS FROM SPECIFIC YEARS?\n1: OVERALL\n2: BASED UPON YEAR";
+        cin >> plyrStat;
+
+        switch (plyrStat) {
+
+
+            //OVERALL STATS
+        case 1:
+            for (int i = 0; i < 10; i++) {
+                //MANDATORY PITCHER SELECTION
+                if (i == 0) {
+                    cout << "Pick one of the following pitchers to add to your team.\n";
+                    //sort func
+                    playerAdder(objects, team);
+                }
+
+                cout << "PICK A POSITION TO CHOOSE A PLAYER FROM\n1: CATCHER\n2: LEFT FIELD\n3: CENTER FIELD\n4: RIGHT FIELD\n5: THIRD BASE\n6: FRIST BASE\n7: SECOND BASE\n8: HOME BASE\n";
+                cin >> plyrPos;
+
+                if (plyrPos <= 1 || plyrPos > 9) {
+                    cout << "Enter a valid input.\n";
+                    cin >> plyrPos;
+                    i--;
+                    continue;
+                }
+
+                //sort function (plyrSortStat, vector<Player> players, 10, best) not overly certain what EXACTLY you all will need for the sort funcs
+                playerAdder(objects, team);
+            }
+            break;
+
+
+            //SPECIFIC STATS
+        case 2:
+            for (int i = 0; i < 10; i++) {
+                //MANDATORY PITCHER SELECTION
+                if (i == 0) {
+                    cout << "Pick one of the following pitchers to add to your team.\n";
+                    //sort func
+                    playerAdder(objects, team);
+                }
+
+                cout << "PICK A POSITION TO CHOOSE A PLAYER FROM\n1: CATCHER\n2: LEFT FIELD\n3: CENTER FIELD\n4: RIGHT FIELD\n5: THIRD BASE\n6: FRIST BASE\n7: SECOND BASE\n8: HOME BASE\n";
+                cin >> plyrPos;
+
+                if (plyrPos <= 1 || plyrPos > 9) {
+                    cout << "Enter a valid input.\n";
+                    cin >> plyrPos;
+                    i--;
+                    continue;
+                }
+
+                //sort function (plyrSortStat, vector<Player> players, 10, best) not overly certain what EXACTLY you all will need for the sort funcs
+                playerAdder(objects, team);
+            }
+            break;
+        }
+    
+    //HE KNOWS TOO MUCH CUZ HE WANNA SEARCH PEOPLE
+    case 2:
+        cout << "WOULD YOU LIKE THE PALYER'S STATS TO BE BASED ON THEIR OVERALL PERFORMANCE OR FROM A SPECIFIC YEAR?\n1:specific\n2: overall\n";
+        cin >> plyrStat;
+
+        switch (plyrStat) {
+
+        //SPECIFIC STATS
+        case 1:
+            for (int i = 0; i < 10; i++) {
+                cout << "PLEASE ENTER A PLAYER'S FIRST AND LAST NAME (First letter of both names capitalized)\n";
+                cin >> plyrNm;
+                cout << "NOW PLEASE ENTER THE YEAR THAT THEY PLAYED\n";
+                cin >> yr;
+                //find the player and add it then clear the objects vector, dunno if we need position played for this
+                //maybe use a generic search func and if not found i.e NULL Player obj then decrement i and do not add anything
+            }
+            break;
+
+        //OVERALL STATS
+        case 2:
+            for (int i = 0; i < 10; i++) {
+                cout << "PLEASE ENTER THE FIRST AND LAST NAME OF THE PLAYER YOU WOULD LIKE TO FIND (First letter of each is capitalized)\n";
+                cin >> plyrNm;
+                //search func call
+            }
+            break;
+
+        //INVALID INPUT
+        default:
+            cout << "Please enter a valid option\n";
+            cin >> plyrStat;
+        }
+        break;
+
+
+    // BEST/WORST STATS FOR TEAMBUILDING
+    case 3:
+        cout << "Do you want the best or worst?\n1:Best\n2:Worst";
+        cin >> bw;
+
+        switch(bw){
+
+        //BEST
+        case 1:
+            cout << "Gimme the best and be rid of the rest. How do you want your stats, overall or based on the performance from a specific year?\n1:Overall\n2:specific\n";
+
+            cin >> plyrStat;
+
+            switch (plyrStat) {
+
+                //OVERALL
+            case 1:
+                cout << "How many people would you like to pick from?\n";
+                cin >> bwNum;
+
+                for (int i = 0; i < 10; i++) {
+                    //MANDATORY PITCHER SELECTION
+                    if (i == 0) {
+                        cout << "Pick one of the following pitchers to add to your team.\n";
+                        //sort func
+                        playerAdder(objects, team);
+                    }
+
+                    cout << "PICK A POSITION TO CHOOSE A PLAYER FROM\n1: CATCHER\n2: LEFT FIELD\n3: CENTER FIELD\n4: RIGHT FIELD\n5: THIRD BASE\n6: FRIST BASE\n7: SECOND BASE\n8: HOME BASE\n";
+                    cin >> plyrPos;
+
+                    if (plyrPos <= 1 || plyrPos > 9) {
+                        cout << "Enter a valid input.\n";
+                        cin >> plyrPos;
+                        i--;
+                        continue;
+                    }
+
+                    //sort function (plyrSortStat, vector<Player> players, bwNum, best) not overly certain what EXACTLY you all will need for the sort funcs
+                    playerAdder(objects, team);
+                }
+                break;
+
+            //SPECIFIC
+            case 2:
+                cout << "How many people would you like to pick from?\n";
+                cin >> bwNum;
+
+                for (int i = 0; i < 10; i++) {
+                    //MANDATORY PITCHER SELECTION
+                    if (i == 0) {
+                        cout << "Pick one of the following pitchers to add to your team.\n";
+                        //sort func
+                        playerAdder(objects, team);
+                    }
+
+                    cout << "PICK A POSITION TO CHOOSE A PLAYER FROM\n1: CATCHER\n2: LEFT FIELD\n3: CENTER FIELD\n4: RIGHT FIELD\n5: THIRD BASE\n6: FRIST BASE\n7: SECOND BASE\n8: HOME BASE\n";
+                    cin >> plyrPos;
+
+                    if (plyrPos <= 1 || plyrPos > 9) {
+                        cout << "Enter a valid input.\n";
+                        cin >> plyrPos;
+                        i--;
+                        continue;
+                    }
+
+                    //sort function (plyrSortStat, vector<Player> players, bwNum, best) not overly certain what EXACTLY you all will need for the sort funcs
+                    playerAdder(objects, team);
+                }
+                break;
+
+            //INVALID INPUT
+            default:
+                cout << "Invalid input\n";
+                cin >> plyrStat;
+            }
+            break;
+
+        //WORST
+        case 2:
+            cout << "Who doesn't love a underdog team? How do you want your stats, overall or specific to a year?\n1:Overall\n2:specific\n";
+
+            cin >> plyrStat;
+
+            switch (plyrStat) {
+
+                //OVERALL
+            case 1:
+                cout << "Alright how many people would you like to pick from?\n";
+                cin >> bwNum;
+
+                for (int i = 0; i < 10; i++) {
+                    //MANDATORY PITCHER SELECTION
+                    if (i == 0) {
+                        cout << "Pick one of the following pitchers to add to your team.\n";
+                        //sort func
+                        playerAdder(objects, team);
+                    }
+
+                    cout << "PICK A POSITION TO CHOOSE A PLAYER FROM\n1: CATCHER\n2: LEFT FIELD\n3: CENTER FIELD\n4: RIGHT FIELD\n5: THIRD BASE\n6: FRIST BASE\n7: SECOND BASE\n8: HOME BASE\n";
+                    cin >> plyrPos;
+
+                    if (plyrPos <= 1 || plyrPos > 9) {
+                        cout << "Enter a valid input.\n";
+                        cin >> plyrPos;
+                        i--;
+                        continue;
+                    }
+
+                    //sort function (plyrSortStat, vector<Player> players, bwNum, best) not overly certain what EXACTLY you all will need for the sort funcs
+                    playerAdder(objects, team);
+                }
+                break;
+                    
+            //SPECIFIC
+            case 2:
+                cout << "Alright how many people would you like to pick from?\n";
+                cin >> bwNum;
+
+                for (int i = 0; i < 10; i++) {
+                    //MANDATORY PITCHER SELECTION
+                    if (i == 0) {
+                        cout << "Pick one of the following pitchers to add to your team.\n";
+                        //sort func
+                        playerAdder(objects, team);
+                    }
+
+                    cout << "PICK A POSITION TO CHOOSE A PLAYER FROM\n1: CATCHER\n2: LEFT FIELD\n3: CENTER FIELD\n4: RIGHT FIELD\n5: THIRD BASE\n6: FRIST BASE\n7: SECOND BASE\n8: HOME BASE\n";
+                    cin >> plyrPos;
+
+                    if (plyrPos <= 1 || plyrPos > 9) {
+                        cout << "Enter a valid input.\n";
+                        cin >> plyrPos;
+                        i--;
+                        continue;
+                    }
+
+                    //sort function (plyrSortStat, vector<Player> players, bwNum, best) not overly certain what EXACTLY you all will need for the sort funcs
+                    playerAdder(objects, team);
+                }
+                break;
+
+            //INVALID INPUT
+            default:
+                cout << "Invalid input\n";
+                cin >> plyrStat;
+            }
+        break;
+        }
+    break;
+
+
+    //INVALID INPUT
+    default:
+        cout << "Please enter a valid selection.\n";
+        cin >> buildQuality;
+    }
+
+}
+
+
+
+
 int main()
 {
+
     //INSTANTIATE VECTORS AND VARIABLES
-    
+
     vector<pair<string, vector<Player>>> positions;   //String holds pos and vector of players in that pos
     vector<Player> playersYears;  //Holds all players individual batting years
     vector<Player> players;  //Holds all players overall stats, batting and pitching if they pitched
     unordered_map<string, int> indexMap;  //Holds map of player ids to ints for constant time access to overall statistics
-    
+
     //LOAD FILES AND IMPORT INFORMATION INTO VECTORS
 
     openFile("People.csv", players, playersYears, indexMap);
     openFile("Batting.csv", players, playersYears, indexMap);
     openFile("Pitching.csv", players, playersYears, indexMap);
-    
+
     //MENU SYSTEM
 
     int choice = 0;
@@ -178,6 +546,9 @@ int main()
 
     }
 
+    // teamBuilder(positions, playersYears, players, team1, name1);
+    // teamBuilder(positions, playersYears, players, team2, name2);
+
     /*
     for (int i = 0; i < players.size(); i++) {
         cout << players.at(i).playerID << " - " << players.at(i).name << " - " << "Games Played: " << players.at(i).gamesPlayed;
@@ -186,11 +557,12 @@ int main()
             cout << "Pitching Stats - ERA: " << players.at(i).ERA << endl;
         }
     }
-    
+
     for (int i = 0; i < playersYears.size(); i++) {
         cout << playersYears.at(i).playerID << " - " << playersYears.at(i).name << " - " << "Games Played: " << playersYears.at(i).gamesPlayed;
         cout << " Team: " << playersYears.at(i).team << " Year: " << playersYears.at(i).year;
         cout << " AB: " << playersYears.at(i).atBats << " H: " << playersYears.at(i).hits << " BA: " << playersYears.at(i).battingAvg << endl;
     }
     */
+
 }
